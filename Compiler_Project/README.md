d02f89c91aaa192e72269250b50a177066c80f01
saminyasareram444 <34398984+saminyasareram444@users.noreply.github.com>
2021-06-07 12:16:13 +0600

    Delete Compiler_Project directory

--- Compiler_Project/README.md
+++
@@ -1,1 +0,0 @@
-# Mini-Compiler-With-Flex-and-Bison
--- Compiler_Project/run.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Users\User\OneDrive\Desktop\Compiler_Project
-bison -d 1707006.y
-flex 1707006.l
-gcc 1707006.tab.c lex.yy.c -o out
-out
-pause
